f6af8f5176bf989fd7de1b82859cb782b4b1c59c
Joey Guerra <github@joeyguerra.com>
2026-05-24 01:02:28 -0500

    feat: add header nav on admin pages

---
+++ pages/admin/_layout.js
@@ -0,0 +1,7 @@
+import { sessionFromRequest } from '../../src/context.js'
+
+export async function data(req) {
+  const session = sessionFromRequest(req)
+  const user = session?.user ?? null
+  return { user }
+}
