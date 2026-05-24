ad41d012e6aa6139c4001cba41e25967c2c785b7
Joey Guerra <github@joeyguerra.com>
2026-05-23 23:35:07 -0500

    fix: browser chrome is showing when i navigate to different channels on iOS

--- docker-build-k3d.sh
+++ docker-build-k3d.sh
@@ -2,6 +2,6 @@
 set -e
 npm version patch --no-git-tag-version
 VERSION=$(node -p "require('./package.json').version")
-sed -i '' -e "s|local/chat-web:[0-9]*\.[0-9]*\.[0-9]*|local/chat-web:$VERSION|g" charts/web/deployment.yaml
+sed -i '' -e "s|local/chat-web:[0-9]*\.[0-9]*\.[0-9]*|local/chat-web:$VERSION|g" charts/web/templates/deployment.yaml
 docker build -t local/chat-web:$VERSION .
 k3d image import local/chat-web:$VERSION -c ${KUBE_CLUSTER:-local}
