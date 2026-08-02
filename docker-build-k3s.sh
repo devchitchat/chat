98a050103bb30db1b2bdd3d831b83df0e0723397
Joey Guerra <github@joeyguerra.com>
2026-08-02 15:34:02 -0500

    fix: need to load the image

--- docker-build-k3s.sh
+++ docker-build-k3s.sh
@@ -2,5 +2,5 @@
 set -e
 VERSION=$(git rev-parse --short HEAD)
 sed -i '' -e "s|local/chat-web:[0-9]*\.[0-9]*\.[0-9]*|local/chat-web:$VERSION|g" charts/web/templates/deployment.yaml
-docker build -t local/chat-web:$VERSION .
+docker build --load -t local/chat-web:$VERSION .
 docker save "local/chat-web:$VERSION" | limactl shell "${LIMA_INSTANCE:-k3s}" -- sudo k3s ctr images import -
