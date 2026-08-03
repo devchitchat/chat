92e708d02e238d2cf089fb1370864b230c55090d
Joey Guerra <github@joeyguerra.com>
2026-08-02 22:37:34 -0500

    fixed: can't find docker socket cuz i moved to colima

--- docker-build-k3s.sh
+++ docker-build-k3s.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+export DOCKER_HOST="${DOCKER_HOST:-unix://${HOME}/.colima/default/docker.sock}"
 VERSION=$(git rev-parse --short HEAD)
 sed -i '' -e "s|local/chat-web:[0-9]*\.[0-9]*\.[0-9]*|local/chat-web:$VERSION|g" charts/web/templates/deployment.yaml
 docker build --load -t local/chat-web:$VERSION .
