12bcedc2f90c52e7aab56254c0fabbd36ce2b919
Joey Guerra <github@joeyguerra.com>
2026-08-04 09:40:13 -0500

    fix: deployment wasn't getting published

--- docker-build-k3s.sh
+++ docker-build-k3s.sh
@@ -2,6 +2,6 @@
 set -e
 export DOCKER_HOST="${DOCKER_HOST:-unix://${HOME}/.colima/default/docker.sock}"
 VERSION=$(git rev-parse --short HEAD)
-sed -i '' -e "s|local/chat-web:[0-9]*\.[0-9]*\.[0-9]*|local/chat-web:$VERSION|g" charts/web/templates/deployment.yaml
+sed -i '' -e "s|local/chat-web:[a-zA-Z0-9]*|local/chat-web:$VERSION|g" charts/web/templates/deployment.yaml
 docker build --load -t local/chat-web:$VERSION .
 docker save "local/chat-web:$VERSION" | limactl shell "${LIMA_INSTANCE:-k3s}" -- sudo k3s ctr images import -
