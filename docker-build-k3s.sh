a1b5565b35fdd323afd5ef4299ca1afe49d1817d
Joey Guerra <github@joeyguerra.com>
2026-08-07 10:10:52 -0500

    fix: channels panel scrolling and pushing wasn't building a newer image

--- docker-build-k3s.sh
+++ docker-build-k3s.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
+
+DOCKER_IMAGE_NAME='local/chat-web'
 export DOCKER_HOST="${DOCKER_HOST:-unix://${HOME}/.colima/default/docker.sock}"
-VERSION=$(git rev-parse --short HEAD)
-sed -i '' -e "s|local/chat-web:[a-zA-Z0-9]*|local/chat-web:$VERSION|g" charts/web/templates/deployment.yaml
-docker build --load -t local/chat-web:$VERSION .
-docker save "local/chat-web:$VERSION" | limactl shell "${LIMA_INSTANCE:-k3s}" -- sudo k3s ctr images import -
+HEAD=$(git rev-parse --short HEAD)
+DEPLOYED=$(grep -o "$DOCKER_IMAGE_NAME:[a-zA-Z0-9._-]*" charts/web/templates/deployment.yaml | head -1 | cut -d: -f2)
+if [ "$HEAD" != "$DEPLOYED" ]; then
+  VERSION=$HEAD
+else
+  VERSION=$(date +%s)
+fi
+sed -i '' -e "s|$DOCKER_IMAGE_NAME:[a-zA-Z0-9._-]*|$DOCKER_IMAGE_NAME:$VERSION|g" charts/web/templates/deployment.yaml
+docker build --load -t $DOCKER_IMAGE_NAME:$VERSION .
+docker save "$DOCKER_IMAGE_NAME:$VERSION" | limactl shell "${LIMA_INSTANCE:-k3s}" -- sudo k3s ctr images import -
